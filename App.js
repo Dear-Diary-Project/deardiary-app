04385b3ef1719ea480a1ba909eacb006117e4b5b
Pruthvi <pruthvishetty5656@gmail.com>
2020-11-20 13:17:39 +0530

    Dear Diary Splash added
    Learnt the basic React Native Components

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight,Linking } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/deardiary.png')
+    }style={{ width: 200, height: 200 }}></Image>
+      <Text style={styles.heading}>Dear Diary</Text>
+      <TouchableHighlight  style={{ width: 50, height: 50,marginTop:220 }} onPress={() => Linking.openURL('https://github.com/Dear-Diary-Project')}>
+      <Image style={{ width: 50, height: 50}} source={require('./assets/github.png')
+    }
+    onPress></Image>
+    </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +25,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading:{
+    fontSize:32,
+    fontWeight:'bold',
+    width:250,
+    textAlign:'center'
+  }
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+# Dear Diary App
+
+### App for Dear Diary made in React Native
+
+<a href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40pruthvi2103/deardiary-app-ed82fa30f85d41e7807828dd709615b1-signed.apk"> APK Link </a>
